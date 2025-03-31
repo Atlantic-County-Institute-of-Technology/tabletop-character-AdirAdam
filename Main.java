4b0eb55787240200b85784ae0b92d181ab7d8af4
Adir <105810536+AdirAdam@users.noreply.github.com>
2025-03-31 13:58:36 -0400

    final code without comments

--- Main.java
+++ Main.java
@@ -1,15 +1,30 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.logging.Level;
 
 public class Main {
 
-    class characterTemplate {
+    public static void main(String[] args) {
+        System.out.println();
+        characterTemplate player = new characterTemplate("Adir", 3);
+        player.addFeat("Chopped");
+        System.out.println(player + "\n");
+
+        Barbs barbarian = new Barbs("Googy", 6);
+        System.out.println(barbarian + "\n");
+
+        Wizard wizard1 = new Wizard("Harry", 1);
+        System.out.println(wizard1 + "\n");
+
+    }
+
+    static class characterTemplate {
         protected String Name;
         protected int STR,DEX,CON,INT,WIS,CHA,LVL,HP,AC;
         ArrayList<String> feats;
 
-        public characterTemplate() {
 
+        public characterTemplate() {
             this.Name = "Player";
             this.STR = this.DEX = this.CON = this.INT = this.WIS = this.CHA = this.HP = this.AC = 10;
             this.LVL = 1;
@@ -17,24 +32,36 @@ public class Main {
         }
 
         public characterTemplate(String name, int level) {
-            Random dice = new Random();
             this.Name = name;
             this.LVL = level;
             this.feats = new ArrayList<String>();
-            String[] statsAvailable = {"STR","DEX","CON","INT","WIS","CHA"};
-            for (int i=0; i < 6; i++) {
-                int[] rolledDice = {dice.nextInt(1,6), dice.nextInt(1,6), dice.nextInt(1,6), dice.nextInt(1,6)};
-                int[] sortedRolledDice = bubbleSort(rolledDice);
+            this.STR = rollAbilityScore();
+            this.DEX = rollAbilityScore();
+            this.CON = rollAbilityScore();
+            this.INT = rollAbilityScore();
+            this.WIS = rollAbilityScore();
+            this.CHA = rollAbilityScore();
+            this.HP = rollAbilityScore();
+            this.AC = rollAbilityScore();
+        }
 
-                for (int j=sortedRolledDice.length-1; j > 1; j--) {
+        private int rollAbilityScore() {
+            Random dice = new Random();
+            int[] rolls = new int[4];
 
-                }
-
-
+            for (int i=0; i < 4; i++) {
+                rolls[i] = dice.nextInt(1,7);
             }
 
+            int[] sortedArray = bubbleSort(rolls);
 
+            int sum = 0;
 
+            for (int i = sortedArray.length-1; i >= 1; i--) {
+                sum += sortedArray[i];
+            }
+
+            return sum;
         }
 
         public static int[] bubbleSort(int[] array) {       // Defines the bubblesort class
@@ -52,10 +79,195 @@ public class Main {
             return array;       // Returns the sorted Array
         }
 
+        public int getAbilityModifier(int score) {
+            return (score - 10) / 2;
+        }
 
+        protected void calculateAC() {
+            AC = 10 + getAbilityModifier(DEX);
+        }
 
+        protected void calculateHP() {
+            int conMod = getAbilityModifier(CON);
+            if (LVL == 1) {
+                HP = 10 + conMod;
+            }
+            else {
+                HP = 10 + conMod + (LVL-1) * (6+conMod);
+            }
+        }
 
+        public void levelUp() {
+            LVL++;
+            calculateHP();
+            calculateAC();
+        }
 
+        public void addFeat(String feat) {
+            feats.add(feat);
+        }
+
+        @Override
+        public String toString() {
+            return Name + " (Level " + LVL + ")" + "\nHP: " + HP + " | AC: " + AC +
+                    "\nSTR: " + STR + " | DEX: " + DEX + " | CON: " + CON +
+                    "\nINT: " + INT + " | WIS: " + WIS + " | CHA: " + CHA +
+                    "\nFeats: " + feats;
+        }
+
+    }
+
+    static class Barbs extends characterTemplate {
+        public Barbs(String name, int level) {
+            super(name,level);
+            STR += 2;
+            CON += 1;
+            feats.add("Rage");
+            feats.add("Unarmored Defense");
+            calculateHP();
+            calculateAC();
+        }
+
+    }
+
+    static class Bards extends characterTemplate {
+        public Bards(String name, int level) {
+            super(name,level);
+            CHA += 2;
+            DEX += 1;
+            feats.add("Inspiration");
+            feats.add("Jack of all Trades");
+            calculateHP();
+            calculateAC();
+        }
+
+    }
+
+    static class Cleric extends characterTemplate {
+        public Cleric(String name, int level) {
+            super(name,level);
+            WIS += 2;
+            CON += 1;
+            feats.add("Channel Divinity");
+            feats.add("Turn Undead");
+            calculateHP();
+            calculateAC();
+        }
+
+    }
+
+    static class Druid extends characterTemplate {
+        public Druid(String name, int level) {
+            super(name,level);
+            WIS += 2;
+            CON += 1;
+            feats.add("Wild Shape");
+            feats.add("Commune w/ Nature");
+            calculateHP();
+            calculateAC();
+        }
+
+    }
+
+    static class Fighter extends characterTemplate {
+        public Fighter(String name, int level) {
+            super(name,level);
+            DEX += 2;
+            CON += 1;
+            feats.add("Channel Divinity");
+            feats.add("Turn Undead");
+            calculateHP();
+            calculateAC();
+        }
+
+    }
+
+    static class Monk extends characterTemplate {
+        public Monk(String name, int level) {
+            super(name,level);
+            DEX += 2;
+            WIS += 1;
+            feats.add("Unarmored Movement");
+            feats.add("Stunning Strike");
+            calculateHP();
+            calculateAC();
+        }
+
+    }
+
+    static class Paladin extends characterTemplate {
+        public Paladin(String name, int level) {
+            super(name,level);
+            STR += 2;
+            CHA += 1;
+            feats.add("Divine Smite");
+            feats.add("Aura of Protection");
+            calculateHP();
+            calculateAC();
+        }
+    }
+
+    static class Ranger extends characterTemplate {
+        public Ranger(String name, int level) {
+            super(name,level);
+            DEX += 2;
+            WIS += 1;
+            feats.add("Favored Enemy");
+            feats.add("Surefooted");
+            calculateHP();
+            calculateAC();
+        }
+
+    }
+
+    static class Rogue extends characterTemplate {
+        public Rogue(String name, int level) {
+            super(name,level);
+            DEX += 2;
+            INT += 1;
+            feats.add("Sneak Attack");
+            feats.add("Uncanny Dodge");
+            calculateHP();
+            calculateAC();
+        }
+
+    }
+
+    static class Sorcerer extends characterTemplate {
+        public Sorcerer(String name, int level) {
+            super(name,level);
+            CHA += 2;
+            CON += 1;
+            feats.add("Wild Magic");
+            feats.add("Flexible Spellcasting");
+            calculateHP();
+            calculateAC();
+        }
+    }
+
+    static class Warlock extends characterTemplate {
+        public Warlock(String name, int level) {
+            super(name,level);
+            CHA += 2;
+            CON += 1;
+            feats.add("Eldrich Patron");
+            feats.add("Pact Magic");
+            calculateHP();
+            calculateAC();
+        }
+
+    }
+
+    static class Wizard extends characterTemplate {
+        public Wizard(String name, int level) {
+            super(name,level);
+            INT += 2;
+            CON += 1;
+            feats.add("Arcane Recovery");
+            feats.add("Spell Mastery");
+            calculateHP();
+            calculateAC();
+        }
 
     }
 }
